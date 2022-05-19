9cc728ff713eab093d4fb5c5dd804c66a1a5447a
kwonshzzang <105833021+kwonshzzang@users.noreply.github.com>
2022-05-19 15:49:35 +0900

    commit

---
+++ src/test/java/kr/co/kwonshzzang/springadmin/SpringAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kwonshzzang.springadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
